29bc42d451a62472003bab3f45479b6c1def2dc3
rchevrier <rchevrier@cls.fr>
2026-07-07 08:30:53 +0000

    Handle ncrc file

--- altimetry_downloader_aviso/auth.py
+++ altimetry_downloader_aviso/auth.py
@@ -2,10 +2,29 @@ import getpass
 import logging
 import netrc
 import os
+import sys
 import warnings
 from pathlib import Path
 
-NETRC_PATH = Path.home() / ".netrc"
+NETRC_PATH = Path.home() / ".altimetry" / ".netrc"
+NCRC_PATH = Path.home() / ".altimetry" / ".ncrc"
+_NCRC_KEY = "HTTP.NETRC"
+_NCRC_SEP = "="
+
+logger = logging.getLogger(__name__)
+
+
+def setup_auth_env():
+    for name in sorted(sys.modules):
+        if name.startswith("netCDF"):
+            msg = (
+                "netCDF4 is already loaded. Authentication configuration may not be "
+                "applied"
+            )
+            warnings.warn(msg)
+
+    os.environ["NCRCENV_RC"] = NCRC_PATH.as_posix()
+    os.environ["NETRC"] = NETRC_PATH.as_posix()
 
 
 class AuthenticationError(Exception):
@@ -29,11 +48,40 @@ def ensure_credentials(host: str):
     AuthenticationError
         In case an exception happens when reading credentials
     """
+    _validate_ncrc_file()
     creds = _get_credentials(host)
     if creds:
         return creds
 
     return _prompt_and_save_credentials(host)
+
+
+def _validate_ncrc_file():
+    NCRC_PATH.parent.mkdir(parents=True, exist_ok=True)
+    NCRC_PATH.touch(exist_ok=True, mode=0o600)
+    update = True
+
+    with open(NCRC_PATH) as f:
+        lines = f.readlines()
+
+    ncrc_entry = f"{_NCRC_KEY}{_NCRC_SEP}{NETRC_PATH.as_posix()}"
+    for ii, line in enumerate(lines):
+        split = line.split(_NCRC_SEP)
+        if split[0] == _NCRC_KEY and split[1] == NETRC_PATH.as_posix():
+            logger.debug("%s entry in %s is up to date.", _NCRC_KEY, NCRC_PATH)
+            update = False
+            break
+        elif split[0] == _NCRC_KEY:
+            logger.debug("Updating existing %s entry in %s", _NCRC_KEY, NCRC_PATH)
+            lines[ii] = ncrc_entry
+            update = False
+            break
+
+    if update:
+        logger.debug("Adding new %s entry in %s", _NCRC_KEY, NCRC_PATH)
+        lines.append(ncrc_entry)
+        with open(NCRC_PATH, mode="w") as f:
+            f.writelines(lines)
 
 
 def _get_credentials(host: str):
@@ -48,7 +96,7 @@ def _get_credentials(host: str):
             return login, password
     except TypeError:
         msg = f"Host {host} doesn't exist in .netrc file."
-        warnings.warn(msg)
+        logger.info(msg)
         return None
     except netrc.NetrcParseError as exc:
         msg = f"Syntax error in .netrc file: {exc}"
